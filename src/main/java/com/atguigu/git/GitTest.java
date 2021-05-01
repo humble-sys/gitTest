e852ee730732f9a336cf29df86ebfeddfe8ff98a
zhenglei <work123458@sina.com>
2021-05-01 12:04:54 +0800

    second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -7,5 +7,6 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("你好 git");
+        System.out.println("你好 git2");
     }
 }
