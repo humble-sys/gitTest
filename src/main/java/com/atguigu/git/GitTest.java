ca76b35259b320609dfd0377e84c590a20becb2f
zhenglei <work123458@sina.com>
2021-05-01 12:05:51 +0800

    third commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("你好 git");
         System.out.println("你好 git2");
+        System.out.println("你好 git3");
     }
 }
